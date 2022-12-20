eeedb517e03a21cbd31592cf81e3268d8627a0bb
Piotr <piotr.wiercigroch1@gmail.com>
2022-12-20 14:46:10 +0100

    gradle update

--- crispyJourney/src/main/java/agh/project/gui/menu/App.java
+++ crispyJourney/src/main/java/agh/project/gui/menu/App.java
@@ -1,4 +1,11 @@
 package agh.project.gui.menu;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
