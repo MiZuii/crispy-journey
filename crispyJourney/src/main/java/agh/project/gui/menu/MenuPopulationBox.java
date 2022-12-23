1a61ba8493936120b521f094f07db22416bcfa71
MiZuii <piotr.wiercigroch1@gmail.com>
2022-12-23 23:00:45 +0100

    style changes + resources added

--- crispyJourney/src/main/java/agh/project/gui/menu/MenuPopulationBox.java
+++ crispyJourney/src/main/java/agh/project/gui/menu/MenuPopulationBox.java
@@ -4,10 +4,22 @@ import agh.project.App.App;
 import agh.project.gui.menu.events.removePopulationEvent;
 import agh.project.gui.menu.events.savePopulationEvent;
 import agh.project.simulation.Population;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.TextAlignment;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.Objects;
 
 /**
  *
@@ -25,6 +37,12 @@ public class MenuPopulationBox extends HBox {
     private final Population population;
     private final Pane parentNode;
     private final App app;
+    // Box elements
+    private HBox filler;
+    private Button deletePopulationButton;
+    private Label populationNameLabel;
+    private Button saveButton;
+    private Button startPopulationSimulationButton;
 
     public MenuPopulationBox(Population population, Pane parent, App app) {
         this.app = app;
@@ -41,19 +59,86 @@ public class MenuPopulationBox extends HBox {
     }
 
     /**
+     * Change populationBox save icon to saved
+     */
+    public void changeSaveIcon() {
+        try {
+            saveButton.setGraphic(getImageViewFromString("save"));
+        }
+        catch (NullPointerException e) {
+            saveButton = new Button("S");
+            System.out.println("Couldn't load image: save");
+        }
+    }
+
+    /**
      * Creates javafx nodes and adds them to itself.
      */
     private void createBox(){
 
         // nodes creation
-        Label populationNameLabel = new Label(population.name);
-        Button deletePopulationButton = new Button("X");
+        filler = new HBox();
+        populationNameLabel = new Label(population.name);
+
+        // delete button
+        deletePopulationButton = saveCreateButton("", "delete", "X");
         deletePopulationButton.setOnAction(new removePopulationEvent(this, parentNode, app));
-        Button saveButton = new Button("S");
+
+        // save button
+        if (app.populationsHolder.getSavedPopulationsNames().contains(population.name)) {
+            saveButton = saveCreateButton("", "save", "S");
+        }
+        else {
+            saveButton = saveCreateButton("", "save-not", "S");
+        }
         saveButton.setOnAction(new savePopulationEvent(this, app));
-        Button startPopulationSimulationButton = new Button(">");
+
+        // start button
+        startPopulationSimulationButton = saveCreateButton("", "play", ">");
+
+        addStyles();
+        addProperties();
 
         // node placing
-        this.getChildren().addAll(populationNameLabel, saveButton, deletePopulationButton, startPopulationSimulationButton);
+        this.getChildren().addAll(populationNameLabel, filler, saveButton, deletePopulationButton, startPopulationSimulationButton);
+    }
+
+    private void addStyles(){
+        try {
+            this.getStylesheets().add(Objects.requireNonNull(this.getClass().getResource("/styles/MenuPopulationBoxStyle.css")).toExternalForm());
+            this.getStyleClass().add("menu-population-box");
+            populationNameLabel.getStyleClass().add("population-name-label");
+        }
+        catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addProperties() {
+        this.setAlignment(Pos.CENTER_RIGHT);
+        this.setPadding(new Insets(15, 0, 5, 0));
+        populationNameLabel.setPadding(new Insets(5, 30, 5, 30));
+        populationNameLabel.setAlignment(Pos.CENTER_LEFT);
+        HBox.setHgrow(filler, Priority.ALWAYS);
+    }
+
+    private Button saveCreateButton(String content, String imgName, String safetyContent) {
+        Button tmpButton;
+        try {
+            tmpButton = new Button(content, getImageViewFromString(imgName));
+        }
+        catch (NullPointerException e) {
+            tmpButton = new Button(safetyContent);
+            System.out.println("Couldn't load image: " + imgName);
+        }
+        return tmpButton;
+    }
+
+    private ImageView getImageViewFromString(String imageName) throws NullPointerException{
+        Image img = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/" + imageName + ".png")));
+        ImageView imgView = new ImageView(img);
+        imgView.setFitHeight(30);
+        imgView.setFitWidth(30);
+        return imgView;
     }
 }
--- crispyJourney/src/main/java/agh/project/gui/menu/MenuScene.java
+++ crispyJourney/src/main/java/agh/project/gui/menu/MenuScene.java
@@ -4,9 +4,18 @@ import agh.project.App.App;
 import agh.project.gui.menu.events.addPopulationEvent;
 import agh.project.interfaces.SceneCreator;
 import agh.project.simulation.Population;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+
+import java.util.Objects;
 
 /**
  * MenuScene is responsible for creating menu javafx scene of the menu
@@ -16,37 +25,117 @@ import javafx.scene.layout.VBox;
  */
 public class MenuScene implements SceneCreator {
 
-    Scene menuScene;
+    private Scene menuScene;
     private VBox root;
+    private VBox populationsContainer;
+    private Label header;
+    private HBox headerBox;
     private Button addPopulationButton;
+    private HBox addPopulationButtonBox;
     private final App app;
 
     public MenuScene(App app) {
         this.app = app;
     }
 
+    /* -------------------------------------------------------------------------- */
+    /*                               Scene creation                               */
+    /* -------------------------------------------------------------------------- */
+
     @Override
     public Scene createScene() {
 
         // nodes creation
+        populationsContainer = new VBox();
         root = new VBox();
+        header = new Label("Populations Simulator");
+        headerBox = new HBox(header);
+        addPopulationButton = saveCreateButton("", "plus", "+");
+        addPopulationButtonBox = new HBox(addPopulationButton);
 
+        // adding pre-saved populations
         for (Population p : app.populationsHolder.getPopulationsList()) {
-            root.getChildren().add(new MenuPopulationBox(p, root, app));
+            populationsContainer.getChildren().add(new MenuPopulationBox(p, populationsContainer, app));
         }
 
-        addPopulationButton = new Button("Add population");
-        addPopulationButton.setOnAction(new addPopulationEvent(this));
-        root.getChildren().add(addPopulationButton);
-        addPopulationButton.toFront();
+        // adding buttons
+        populationsContainer.getChildren().add(addPopulationButtonBox);
+        root.getChildren().addAll(headerBox, populationsContainer);
 
+        // create scene
         menuScene = new Scene(root, 600, 600);
+
+        addProperties();
+        addStyles();
+
         return menuScene;
     }
 
+    private void addStyles() {
+        try {
+            root.getStylesheets().add(Objects.requireNonNull(this.getClass().getResource("/styles/MenuSceneStyle.css")).toExternalForm());
+            root.getStyleClass().add("root");
+            header.getStyleClass().add("header-label");
+            headerBox.getStyleClass().add("header-box");
+            populationsContainer.getStyleClass().add("populations-container");
+            addPopulationButton.getStyleClass().add("add-population-button");
+        }
+        catch (NullPointerException e) {
+            System.out.println("Menu scene style sheet couldn't have been loaded.");
+            e.printStackTrace();
+        }
+    }
+
+    private void addProperties() {
+        // root properties
+        root.prefWidthProperty().bind(menuScene.widthProperty());
+        root.prefHeightProperty().bind(menuScene.heightProperty());
+        root.setAlignment(Pos.TOP_CENTER);
+
+        // population container
+        populationsContainer.setMaxWidth(600);
+        VBox.setVgrow(populationsContainer, Priority.ALWAYS);
+
+        // add population button
+        addPopulationButton.setPadding(new Insets(10, 10, 10, 10));
+
+        // buttons
+        addPopulationButton.setOnAction(new addPopulationEvent(this));
+        addPopulationButtonBox.setAlignment(Pos.CENTER_RIGHT);
+        addPopulationButtonBox.setPadding(new Insets(10, 10, 10, 10));
+
+        // header
+        HBox.setHgrow(headerBox, Priority.ALWAYS);
+        headerBox.setPadding(new Insets(10, 0, 10, 20));
+    }
+
+    private Button saveCreateButton(String content, String imgName, String safetyContent) {
+        Button tmpButton;
+        try {
+            tmpButton = new Button(content, getImageViewFromString(imgName));
+        }
+        catch (NullPointerException e) {
+            tmpButton = new Button(safetyContent);
+            System.out.println("Couldn't load image: " + imgName);
+        }
+        return tmpButton;
+    }
+
+    private ImageView getImageViewFromString(String imageName) throws NullPointerException{
+        Image img = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/" + imageName + ".png")));
+        ImageView imgView = new ImageView(img);
+        imgView.setFitHeight(30);
+        imgView.setFitWidth(30);
+        return imgView;
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                            Public functionalities                          */
+    /* -------------------------------------------------------------------------- */
+
     public void addNewPopulation(Population population) {
-        root.getChildren().add(new MenuPopulationBox(population, root, app));
-        addPopulationButton.toFront();
+        populationsContainer.getChildren().add(new MenuPopulationBox(population, populationsContainer, app));
+        addPopulationButtonBox.toFront();
         app.populationsHolder.addPopulation(population);
     }
 
